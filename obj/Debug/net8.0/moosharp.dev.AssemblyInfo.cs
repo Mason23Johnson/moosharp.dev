6988d0021c5602fc2cc7ee80da314473b5aa2adf
Mason23Johnson <masonbjohnson@hotmail.com>
2025-05-28 03:17:42 -0500

    scoreboard fixes and condensing

--- obj/Debug/net8.0/moosharp.dev.AssemblyInfo.cs
+++ obj/Debug/net8.0/moosharp.dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab736c6e30a0f81fe54205419dddc92a418397d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ed592bc349f377342049ac884e94b237f13a25")]
 [assembly: System.Reflection.AssemblyProductAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
