f57683bb18783a3cefff0509e8df90bfe86d4b82
Mason23Johnson <masonbjohnson@hotmail.com>
2025-05-29 03:38:47 -0500

    misc

--- obj/Debug/net8.0/moosharp.dev.AssemblyInfo.cs
+++ obj/Debug/net8.0/moosharp.dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0ed592bc349f377342049ac884e94b237f13a25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6988d0021c5602fc2cc7ee80da314473b5aa2adf")]
 [assembly: System.Reflection.AssemblyProductAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
