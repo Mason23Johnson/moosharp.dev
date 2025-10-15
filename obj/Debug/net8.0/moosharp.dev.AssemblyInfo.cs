3b8dcca515c4c76b349ec57e0b665a2f4110cf9d
Mason23Johnson <masonbjohnson@hotmail.com>
2025-10-15 00:23:15 -0500

    fix?

--- obj/Debug/net8.0/moosharp.dev.AssemblyInfo.cs
+++ obj/Debug/net8.0/moosharp.dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11bd1240af2ea3d89d3c142243a2faebfbcafeb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258c743b1c564972d22a22b17fa6379763b883bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moosharp.dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
